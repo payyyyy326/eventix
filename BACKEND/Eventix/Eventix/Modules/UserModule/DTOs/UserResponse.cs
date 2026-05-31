ab3512fc0231fb230273b1f0e7672aa27b7b22c2
Phuoc Nguyen Huy <huyphuoc204@gmail.com>
2026-05-31 15:45:52 +0700

    Reapply "change name file"
    This reverts commit b2d5bb565db074427c683f99adcd61a25df607ae.

---
+++ BACKEND/Eventix/Eventix/Modules/UserModule/DTOs/UserResponse.cs
@@ -0,0 +1,32 @@
+﻿using Eventix.Entities;
+
+namespace Eventix.Modules.UserModule.DTOs
+{
+    public class UserResponse
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public string FullName { get; set; } = null!;
+
+        public string? PhoneNumber { get; set; }
+
+        public string? AvatarUrl { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public bool EmailVerified { get; set; }
+
+        public DateTime? EmailVerifiedAt { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; } = new List<Role>();
+
+    }
+}
---
+++ BACKEND/Eventix/Eventix/Modules/UserModule/DTOs/UserUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Eventix.Modules.UserModule.DTOs
+{
+    public class UserUpdateRequest
+    {
+        public string FullName { get; set; } = null!;
+
+        public string? PhoneNumber { get; set; }
+
+        public string? AvatarUrl { get; set; }
+
+    }
+}
