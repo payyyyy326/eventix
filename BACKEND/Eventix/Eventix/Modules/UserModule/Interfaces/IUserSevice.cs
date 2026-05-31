28dc6895c82524015e29d758469a228c013ce83a
Phuoc Nguyen Huy <huyphuoc204@gmail.com>
2026-05-31 15:45:14 +0700

    change name file

---
+++ BACKEND/Eventix/Eventix/Modules/UserModule/Interfaces/IUserSevice.cs
@@ -0,0 +1,17 @@
+﻿using Eventix.Modules.UserModule.DTOs;
+
+namespace Eventix.Modules.UserModule.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<UserResponse> GetUserByIdAsync(Guid userId);
+
+        public Task<UserResponse> GetUserByEmailAsync(string email);
+
+        public Task<IEnumerable<UserResponse>> GetAllUsersAsync();
+
+        public Task<UserResponse> UpdateUserAsync(Guid userId, UserUpdateRequest request);
+
+        public Task<bool> DeleteUserAsync(Guid userId);
+    }
+}
